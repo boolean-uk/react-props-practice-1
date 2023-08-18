9bcb1f9b1bcfc5091f603d050b16c42a20e51901
Annefleur Hay <annefleurhay1@gmail.com>
2023-08-18 10:09:59 +0200

    props assignment 1

---
+++ src/NameLength.js
@@ -0,0 +1,7 @@
+function NameLength(props) {
+    return <p className="name-length">
+    Did you know that {props.name} is {props.name.length} characters long?!
+  </p>
+}
+
+export default NameLength
