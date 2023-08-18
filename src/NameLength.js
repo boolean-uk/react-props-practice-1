305ba8fab8efe3ada92c2be8d6e6f76b863dcbc8
Esther <estherbuijnsters@gmail.com>
2023-08-18 10:10:10 +0200

    EstherB finished react-props1

---
+++ src/NameLength.js
@@ -0,0 +1,7 @@
+function NameLenght (props) {
+    return <p className="name-length">
+    Did you know that {props.name} is {props.name.length} characters long?!
+  </p>
+}
+
+export default NameLenght
---
+++ src/Title.js
@@ -0,0 +1,4 @@
+function Title (props) {
+    return (<h1 className="title">Welcome, {props.name}</h1>)
+}
+export default Title
