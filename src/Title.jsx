1cf7df44a6fc4f8c5d70a4695140aebbd55923ed
Annemoon_de_Groen <annemoon2003@gmail.com>
2024-02-22 09:56:43 +0100

    Finished exercise

---
+++ src/Title.jsx
@@ -0,0 +1,14 @@
+import PropTypes from "prop-types"
+
+function Title({name})
+{
+    return (
+        <h1 className="title">Welcome, {name}!</h1>
+
+    )
+}
+
+Title.propTypes = {
+    name:PropTypes.string
+}
+export default Title
