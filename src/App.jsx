e651920692132fcc356574610bfb7b487496c9f1
Pia Loana Mullenbach <pia.loana.mullenbach@dk.experis.com>
2024-02-22 09:57:59 +0100

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
-import { useState } from 'react'
-import { reverse } from './helpers'
+import { useState } from "react";
+//import { reverse } from "./helpers";
+import NameLength from "./nameLength";
+import NameReversed from "./nameReversed";
+import Title from "./Title";
 
 function App() {
-  const [name, setName] = useState('Nicolas')
+  const [name, setName] = useState("Nicolas");
 
   return (
     <div className="app">
-      <h1 className="title">Welcome, {name}!</h1>
-      <p className="name-length">
-        Did you know that {name} is {name.length} characters long?!
-      </p>
-      <p className="name-reversed">
-        Also, {name} backwards is {reverse(name)}
-      </p>
+      <section>
+        <Title name={name} />
+      </section>
+      <section>
+        <NameLength name={name} />
+      </section>
+      <section>
+        <NameReversed name={name} />
+      </section>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Title.jsx
@@ -0,0 +1,11 @@
+import PropTypes from "prop-types";
+
+function Title({ name }) {
+  return <h1 className="title">Welcome, {name}!</h1>;
+}
+
+Title.propTypes = {
+  name: PropTypes.string,
+};
+
+export default Title;
