31450729c4a49c552652d995c76cc2ff9f5545f6
Sini Suihkonen <sahsuihkonen@gmail.com>
2024-02-22 10:28:36 +0200

    done

---
+++ src/NameLenght.jsx
@@ -0,0 +1,17 @@
+import PropTypes from "prop-types";
+
+function NameLenght(props) {
+  const { name } = props;
+
+  return (
+    <p className="name-length">
+      Did you know that {name} is {name.length} characters long?!
+    </p>
+  );
+}
+
+NameLenght.propTypes = {
+  name: PropTypes.string,
+};
+
+export default NameLenght;
