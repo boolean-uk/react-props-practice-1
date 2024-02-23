8d580287749b077c8766bf9fa883271c75d6366a
Mahmoud Ali <mahmoodali2965.ma@gmail.com>
2024-02-23 10:00:42 +0100

    Done

---
+++ src/Components/NameReversed.jsx
@@ -0,0 +1,14 @@
+import PropTypes from "prop-types";
+import { reverse } from "../helpers";
+
+export default function NameReversed({ name }) {
+  return (
+    <p className="name-reversed">
+      Also, {name} backwards is {reverse(name)}
+    </p>
+  );
+}
+
+NameReversed.propTypes = {
+  name: PropTypes.string.isRequired,
+};
