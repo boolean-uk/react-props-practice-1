082aa286d6a7eb0bfdde12dd4421c31ef28c85a5
Aziz Zafar <aziz.zafar@no.experis.com>
2024-02-23 10:58:11 +0100

    Aziz Zafar /C#EX3

---
+++ src/components/NameReversed.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { reverse } from '../helpers'
+function NameReversed({name}) {
+  return (
+    <p className="name-reversed">
+        Also, {name} backwards is {reverse(name)}
+      </p>
+  )
+}
+
+export default NameReversed
