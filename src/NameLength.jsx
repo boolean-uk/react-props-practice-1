e5cb84089d01ad691c89d1b82348a4312e60b10c
homonoviscoding <mama.leye@gmail.com>
2024-04-25 11:17:24 +0100

    Props Practice 1

---
+++ src/NameLength.jsx
@@ -0,0 +1,7 @@
+export default function nameLength(name) {
+    return (
+        <p className="name-length">
+        Did you know that {name.name} is {name.name.length} characters long?!
+      </p>
+    )
+}
---
+++ src/Title.jsx
@@ -0,0 +1,5 @@
+export default function title(name) {
+    return (
+        <h1 className="title">Welcome, {name.name}!</h1>
+    )
+}
